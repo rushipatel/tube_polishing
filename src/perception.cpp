4285b189d1ab5648ee0d9c5f4b13aad1889da783
Rushi Patel <rushipatel89@gmail.com>
2013-10-20 20:16:23 -0400

    cylindr filter added in perception.cpp

--- src/perception.cpp
+++ src/perception.cpp
@@ -1,5 +1,10 @@
 #include "perception.h"
 
+struct line
+{
+    PointT p1;
+    PointT p2;
+};
 
 //hase to contain normals
 typedef pcl::PointXYZRGBNormal PointT;
@@ -177,6 +182,124 @@ void get_axis_points(pcl::PointCloud<PointT>::Ptr cloud_in, double r, pcl::Point
     get_largest_cluster(raw_axis_points, axis_points);
 
 }
+//-----------------------------------------------------------------------------
+// Name: CylTest_CapsFirst
+// Orig: Greg James - gjames@NVIDIA.com
+// Lisc: Free code - no warranty & no money back.  Use it all you want
+// Desc:
+//    This function tests if the 3D point 'testpt' lies within an arbitrarily
+// oriented cylinder.  The cylinder is defined by an axis from 'pt1' to 'pt2',
+// the axis having a length squared of 'lengthsq' (pre-compute for each cylinder
+// to avoid repeated work!), and radius squared of 'radius_sq'.
+//    The function tests against the end caps first, which is cheap -> only
+// a single dot product to test against the parallel cylinder caps.  If the
+// point is within these, more work is done to find the distance of the point
+// from the cylinder axis.
+//    Fancy Math (TM) makes the whole test possible with only two dot-products
+// a subtract, and two multiplies.  For clarity, the 2nd mult is kept as a
+// divide.  It might be faster to change this to a mult by also passing in
+// 1/lengthsq and using that instead.
+//    Elminiate the first 3 subtracts by specifying the cylinder as a base
+// point on one end cap and a vector to the other end cap (pass in {dx,dy,dz}
+// instead of 'pt2' ).
+//
+//    The dot product is constant along a plane perpendicular to a vector.
+//    The magnitude of the cross product divided by one vector length is
+// constant along a cylinder surface defined by the other vector as axis.
+//
+// Return:  -1.0 if point is outside the cylinder
+// Return:  distance squared from cylinder axis if point is inside.
+//
+//-----------------------------------------------------------------------------
+//   http://www.flipcode.com/archives/Fast_Point-In-Cylinder_Test.shtml
+
+/*struct Vec3
+{
+    float x;
+    float y;
+    float z;
+};*/
+
+float is_in_cylinder( const PointT & pt1, const PointT & pt2, float length_sq, float radius_sq, const PointT & testpt )
+{
+    float dx, dy, dz;	// vector d  from line segment point 1 to point 2
+    float pdx, pdy, pdz;	// vector pd from point 1 to test point
+    float dot, dsq;
+
+    dx = pt2.x - pt1.x;	// translate so pt1 is origin.  Make vector from
+    dy = pt2.y - pt1.y;     // pt1 to pt2.  Need for this is easily eliminated
+    dz = pt2.z - pt1.z;
+
+    pdx = testpt.x - pt1.x;		// vector from pt1 to test point.
+    pdy = testpt.y - pt1.y;
+    pdz = testpt.z - pt1.z;
+
+    // Dot the d and pd vectors to see if point lies behind the
+    // cylinder cap at pt1.x, pt1.y, pt1.z
+
+    dot = pdx * dx + pdy * dy + pdz * dz;
+
+    // If dot is less than zero the point is behind the pt1 cap.
+    // If greater than the cylinder axis line segment length squared
+    // then the point is outside the other end cap at pt2.
+
+    if( dot < 0.0f || dot > length_sq )
+    {
+        return( -1.0f );
+    }
+    else
+    {
+        // Point lies within the parallel caps, so find
+        // distance squared from point to line, using the fact that sin^2 + cos^2 = 1
+        // the dot = cos() * |d||pd|, and cross*cross = sin^2 * |d|^2 * |pd|^2
+        // Carefull: '*' means mult for scalars and dotproduct for vectors
+        // In short, where dist is pt distance to cyl axis:
+        // dist = sin( pd to d ) * |pd|
+        // distsq = dsq = (1 - cos^2( pd to d)) * |pd|^2
+        // dsq = ( 1 - (pd * d)^2 / (|pd|^2 * |d|^2) ) * |pd|^2
+        // dsq = pd * pd - dot * dot / lengthsq
+        //  where lengthsq is d*d or |d|^2 that is passed into this function
+
+        // distance squared to the cylinder axis:
+
+        dsq = (pdx*pdx + pdy*pdy + pdz*pdz) - dot*dot/length_sq;
+
+        if( dsq > radius_sq )
+        {
+            return( -1.0f );
+        }
+        else
+        {
+            return( dsq );		// return distance squared to axis
+        }
+    }
+}
+
+void points_in_cylinder(line l, pcl::PointCloud<PointT>::Ptr cloud_in, pcl::PointIndices::Ptr inliers)
+{
+    PointT p1, p2;
+    p1 = l.p1;
+    p2 = l.p2;
+    int pts_cnt = 0;
+    float dx = p1.x - p2.x;
+    float dy = p1.y - p2.y;
+    float dz = p1.z - p2.z;
+
+    float r = 0.01;
+
+    float l_sq = (dx*dx) + (dy*dy) + (dz*dz);
+    float r_sq = r*r;
+
+    for(size_t i=0; i<cloud_in->points.size(); i++)
+    {
+        if(is_in_cylinder(p1, p2, l_sq, r_sq, cloud_in->points[i])>0)
+        {
+            inliers->indices.push_back(i);
+            pts_cnt++;
+        }
+    }
+    ROS_INFO("%d Points found in cylinder filter",pts_cnt);
+}
 
 void remove_inliers(pcl::PointCloud<PointT>::Ptr points, pcl::PointIndices::Ptr indices)
 {
@@ -204,6 +327,11 @@ void remove_inliers(pcl::PointCloud<PointT>::Ptr points,  std::vector<int> &indi
 
 bool ransac_line(pcl::PointCloud<PointT>::Ptr axis_points, pcl::ModelCoefficients::Ptr coefficient, pcl::PointIndices::Ptr inliers)
 {
+    if(axis_points->points.size()<20) //check if there are enough points to generate model
+    {
+        ROS_INFO("No more points to fit line model");
+        return false;
+    }
     pcl::SACSegmentation<PointT> seg;
     pcl::ModelCoefficients coeff;
     //pcl::PointIndices inliers;
@@ -215,25 +343,23 @@ bool ransac_line(pcl::PointCloud<PointT>::Ptr axis_points, pcl::ModelCoefficient
     seg.setOptimizeCoefficients(true);
     seg.setProbability(0.99);
     seg.setMaxIterations (10000);
-    seg.setDistanceThreshold (0.005);
+    seg.setDistanceThreshold (0.0005);  //This will determine smallest cylinder
     seg.setInputCloud (axis_points);
-
     // Obtain the cylinder inliers and coefficients
     seg.segment (*inliers, coeff);
-
     coefficient->header = coeff.header;
     coefficient->values = coeff.values;
 
     ROS_INFO("Number of Line inliers : %d",inliers->indices.size());
     ROS_INFO("Line coefficients are: [X= %f Y=%f Z=%f] [N_X=%f N_Y=%f N_Z=%f]", coeff.values[0],coeff.values[1],coeff.values[2],coeff.values[3],coeff.values[4],coeff.values[5]);
 
-    if(inliers->indices.size()>100)
+    if(inliers->indices.size()>20) // if confidence in line
         return true;
     else
         return false;
 }
 
-void get_line_points(pcl::PointCloud<PointT>::Ptr axis_points, pcl::PointIndices::Ptr inliers, pcl::PointCloud<PointT>::Ptr axis_line_points, pcl::ModelCoefficients::Ptr line_coeff)
+void get_line_points(pcl::PointCloud<PointT>::Ptr axis_points, pcl::PointIndices::Ptr inliers, pcl::PointCloud<PointT>::Ptr axis_line_points, pcl::ModelCoefficients::Ptr line_coeff, line &ln)
 {
     pcl::PointCloud<PointT>::Ptr line_points(new pcl::PointCloud<PointT>);
     pcl::PointCloud<PointT>::Ptr line_inliers(new pcl::PointCloud<PointT>);
@@ -250,9 +376,12 @@ void get_line_points(pcl::PointCloud<PointT>::Ptr axis_points, pcl::PointIndices
     proj.filter(*line_points);
     PointT p1, p2;
     pcl::getMaxSegment(*line_points,p1,p2);
-    axis_line_points->points.push_back(p1);
-    axis_line_points->points.push_back(p2);
-    //pcl::concatenateFields();
+
+    ln.p1 = p1;
+    ln.p2 = p2;
+
+    for(size_t i=0; i<line_points->points.size(); i++)
+        axis_line_points->points.push_back(line_points->points[i]);
 }
 
 
@@ -260,33 +389,27 @@ int segmentize_axis(pcl::PointCloud<PointT>::Ptr axis_points, pcl::PointCloud<Po
 {
     pcl::ModelCoefficients::Ptr line_coeff(new pcl::ModelCoefficients);
     pcl::PointIndices::Ptr inliers(new pcl::PointIndices);
-    //pcl::PointCloud<PointT>::Ptr axis_line_points (new pcl::PointCloud<PointT>);
+    std::vector<line> ln_vec;
+    line ln;
+    int idx;
 
-    /*pcl::PointCloud<PointT>::Ptr filtered_axis_points(new pcl::PointCloud<PointT>);
-    pcl::RadiusOutlierRemoval<PointT> outrem;
-    outrem.setRadiusSearch(0.003);
-    outrem.setMinNeighborsInRadius(2);
-    outrem.filter(*filtered_axis_points);
-    ROS_INFO("%d Outliers removed",axis_points->points.size()-filtered_axis_points->points.size());*/
-
-    PointT p1, p2;
-    bool flag = true;
-    do
+    while(ransac_line(axis_points,line_coeff,inliers))
     {
-        inliers->indices.clear();
-        flag = false;
-        if(ransac_line(axis_points,line_coeff,inliers))
-            flag = true;
-//        p.x = line_coeff->values[0];
-//        p.y = line_coeff->values[1];
-//        p.z = line_coeff->values[2];
-        get_line_points(axis_points,inliers,axis_line_points,line_coeff);
+        get_line_points(axis_points,inliers,axis_line_points,line_coeff,ln);
         remove_inliers(axis_points,inliers);
-    }while(flag);
+        ln_vec.push_back(ln);
+        inliers->indices.clear();
+        points_in_cylinder(ln, axis_points, inliers);
+        remove_inliers(axis_points,inliers);
+
+        inliers->indices.clear();
+    }
+
     axis_line_points->header = axis_points->header;
     axis_line_points->width = axis_line_points->points.size();
     axis_line_points->height = 1;
-    ROS_INFO("%d lines found",axis_line_points->points.size());
+    ROS_INFO("%d axis points found",axis_line_points->points.size());
+    ROS_INFO("%d Lines found", ln_vec.size());
 }
 
 void process_tube_cloud(sensor_msgs::PointCloud2 &object_cloud)
@@ -315,7 +438,7 @@ void process_tube_cloud(sensor_msgs::PointCloud2 &object_cloud)
         cloud->points[i].rgb = cloud2->points[i].rgb;
     }
     estimate_normals(cloud);
-    display_normals(cloud);
+    //display_normals(cloud);
     r = get_radius(cloud);
 
     pcl::PointCloud<PointT>::Ptr axis_points(new pcl::PointCloud<PointT>);
